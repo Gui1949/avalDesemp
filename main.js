b83b186445356872ef3b7a98f94bfeb4e2b84c95
Gui1949 <guilhermesantiago12@gmail.com>
2022-10-26 17:46:15 -0300

    Dashboard e inicio da resposta

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ app.post("/login", (req, res) => {
       request.query(
         `SELECT CODFUNC FROM FUNCIONARIO WHERE EMAILFUNC = '${email}' AND SENHA = '${senha}'`,
         function (err, recordset) {
+          console.log(recordset);
           res.json(recordset.recordsets[0][0]);
         }
       );
@@ -56,11 +57,62 @@ app.post("/formulario", (req, res) => {
     .catch((err) => console.log("erro! " + err));
 });
 
+app.post("/resposta", (req, res) => {
+  console.log(req.body);
+
+  sql
+    .connect(connStr)
+    .then((conn) => {
+      console.log("conectou!");
+
+      let request = new sql.Request();
+      request.query(
+        `SELECT NOMEPIL, NOMECARA, RESPOSTA  FROM AVAITEM ITE
+        INNER JOIN CARACTERISTICA CARA ON CARA.CODCARA  = ITE.CODCARA
+        INNER JOIN PILAR PIL ON PIL.CODPIL = CARA.CODPIL
+        INNER JOIN AVAPEN PEN ON PEN.CODAVA = ITE.CODAVA
+        WHERE CODPEN = 18`,
+        function (err, recordset) {
+          res.json(recordset.recordsets[0]);
+        }
+      );
+    })
+    .catch((err) => console.log("erro! " + err));
+});
+
+app.post("/pendentes", (req, res) => {
+  console.log(req.body);
+  let codfunc = req.body.codfunc;
+
+  sql
+    .connect(connStr)
+    .then((conn) => {
+      console.log("conectou!");
+
+      let request = new sql.Request();
+      request.query(
+        `SELECT CODPEN, FUNC.NOMEFUNC, CASE WHEN STATUS = 0 THEN 'PENDENTE' ELSE 'REALIZADO' END AS STATUS, FORMAT(DATAINS, 'd', 'pt-br') AS DATAINS, 
+        FORMAT(CAB.DTINC, 'd', 'pt-br') AS DTINC
+        FROM AVAPEN PEN
+        INNER JOIN FUNCIONARIO FUNC ON FUNC.CODFUNC = PEN.CODFUNC
+        LEFT JOIN AVACAB CAB ON CAB.CODAVA = PEN.CODAVA
+        WHERE PEN.CODREF = ${codfunc}`,
+        function (err, recordset) {
+          try {
+            res.json(recordset.recordsets[0]);
+          } catch {}
+        }
+      );
+    })
+    .catch((err) => console.log("erro! " + err));
+});
+
 app.post("/send_formulario", (req, res) => {
   console.log(req.body);
 
   let corpo = req.body.dados;
   let codfunc = req.body.codfunc;
+  let codpen = req.body.codpen;
 
   //inserir
 
@@ -100,7 +152,9 @@ app.post("/send_formulario", (req, res) => {
                         let request = new sql.Request();
 
                         request.query(
-                          `INSERT INTO AVAITEM (CODAVA, CODCARA, RESPOSTA) VALUES (${codava}, ${key}, ${value})`,
+                          `INSERT INTO AVAITEM (CODAVA, CODCARA, RESPOSTA) VALUES (${codava}, ${key}, ${value})
+                          UPDATE AVAPEN SET CODAVA = ${codava}, STATUS = 1 WHERE CODPEN = ${codpen}
+                          `,
                           function (err, recordset) {
                             console.log(recordset);
                           }
