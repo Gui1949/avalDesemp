d5be3056dffed342909157d47a7ddb78bf95eb68
Gui1949 <guilhermesantiago12@gmail.com>
2022-11-09 17:31:24 -0300

    Alteração de cadastro de funcionário

--- main.js
+++ main.js
@@ -6,13 +6,50 @@ const bodyParser = require("body-parser");
 const md5 = require("md5");
 const cors = require("cors");
 const { response } = require("express");
+const crypto = require("crypto");
 
 const connStr =
-  "Server=10.10.10.5,1449;Database=AV_DESEMP;User Id=MEDSYSTEMS\\guilherme.floriano;Password=MickJa68@;trustServerCertificate=true";
+  "Server=10.10.10.5,1449;Database=AV_DESEMP;User Id=MEDSYSTEMS\\guilherme.floriano;Password=MickJa69@;trustServerCertificate=true";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+let sessao = () => {
+  let token = crypto.randomBytes(64).toString("hex");
+  return token;
+};
+
+let criar_sessao = (codfunc, token) => {
+  sql.connect(connStr).then((conn) => {
+    let request = new sql.Request();
+
+    console.log("conectou!");
+    request.query(
+      `INSERT INTO SESSAO VALUES ('${token}', ${codfunc}, GETDATE())`,
+      function (err, recordset) {
+        console.log(recordset);
+      }
+    );
+  });
+};
+
+let job_sessao = () => {
+  sql.connect(connStr).then((conn) => {
+    console.log("conectou!");
+
+    let request = new sql.Request();
+    request.query(
+      `DELETE FROM SESSAO WHERE DATAINC < DATEADD(hour, -24, GETDATE())`,
+      function (err, recordset) {
+        let resposta = recordset.recordsets[0];
+        console.log("JOB APAGADOR DE SESSAO: ", resposta);
+      }
+    );
+  });
+};
+
+setInterval(() => job_sessao(), 3600000);
 
 app.post("/login", (req, res) => {
   console.log(req.body);
@@ -29,8 +66,39 @@ app.post("/login", (req, res) => {
       request.query(
         `SELECT CODFUNC, TIPOID, CODGESTOR FROM FUNCIONARIO WHERE EMAILFUNC = '${email}' AND SENHA = '${senha}'`,
         function (err, recordset) {
+          console.log(recordset.recordsets[0][0]);
+          let resposta = recordset.recordsets[0][0];
+          resposta.token = sessao();
+          criar_sessao(resposta.CODFUNC, resposta.token);
+          res.json(resposta);
+        }
+      );
+    })
+    .catch((err) => console.log("erro! " + err));
+});
+
+app.post("/valida", (req, res) => {
+  console.log(req.body);
+
+  let token = req.body.token;
+
+  sql
+    .connect(connStr)
+    .then((conn) => {
+      console.log("conectou!");
+
+      let request = new sql.Request();
+      request.query(
+        `SELECT * FROM SESSAO WHERE TOKEN = '${token}'`,
+        function (err, recordset) {
           console.log(recordset);
-          res.json(recordset.recordsets[0][0]);
+          let resposta = recordset.recordsets[0][0];
+          console.log(resposta);
+          if (!resposta) {
+            res.send(401);
+          } else {
+            res.json(resposta);
+          }
         }
       );
     })
@@ -153,11 +221,9 @@ app.post("/func_aval", (req, res) => {
         INNER JOIN FUNCIONARIO FUNC ON FUNC.CODFUNC = PEN.CODFUNC
         INNER JOIN FUNCIONARIO FUNC1 ON FUNC1.CODFUNC = PEN.CODREF
         WHERE 
-        DATEPART(HOUR,DATAINS) = (SELECT MAX(DATEPART(HOUR,DATAINS)) FROM AVAPEN) AND
         DATEPART(YEAR,DATAINS) = (SELECT MAX(DATEPART(YEAR,DATAINS)) FROM AVAPEN) AND
         DATEPART(MONTH,DATAINS) = (SELECT MAX(DATEPART(MONTH,DATAINS)) FROM AVAPEN) AND
-        DATEPART(DAY,DATAINS) = (SELECT MAX(DATEPART(DAY,DATAINS)) FROM AVAPEN) AND
-        DATEPART(MINUTE,DATAINS) = (SELECT MAX(DATEPART(MINUTE,DATAINS)) FROM AVAPEN)
+        DATEPART(DAY,DATAINS) = (SELECT MAX(DATEPART(DAY,DATAINS)) FROM AVAPEN)
         AND PEN.CODFUNC <> PEN.CODREF
         `,
         function (err, recordset) {
@@ -166,7 +232,121 @@ app.post("/func_aval", (req, res) => {
       );
     })
     .catch((err) => console.log("erro! " + err));
-})
+});
+
+app.post("/novo_usu", (req, res) => {
+  console.log(req.body);
+  let dados = req.body;
+
+  console.log(dados);
+
+  res.json({ data: null });
+
+  sql
+    .connect(connStr)
+    .then((conn) => {
+      console.log("conectou!");
+
+      let request = new sql.Request();
+
+      console.log(
+        `${
+          dados.Nome
+            ? "UPDATE FUNCIONARIO SET NOMEFUNC = '" +
+              dados.Nome +
+              `' WHERE CODFUNC = ${dados.codfunc} `
+            : ""
+        }` +
+          `${
+            dados.Email
+              ? "UPDATE FUNCIONARIO SET EMAILFUNC = '" +
+                dados.Email +
+                `' WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }` +
+          `${
+            dados.Departamento
+              ? `UPDATE FUNCIONARIO SET CODDEP = (SELECT CODDEP FROM DEPARTAMENTO WHERE NOMEDEP ='${dados.Departamento}') WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }` +
+          `${
+            dados.Cargo
+              ? `UPDATE FUNCIONARIO SET CODCAR = (SELECT CODCAR FROM CARGO WHERE NOMECAR = '${dados.Cargo}') WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }`
+      );
+
+      request.query(
+        `${
+          dados.Nome
+            ? "UPDATE FUNCIONARIO SET NOMEFUNC = '" +
+              dados.Nome +
+              `' WHERE CODFUNC = ${dados.codfunc} `
+            : ""
+        }` +
+          `${
+            dados.Email
+              ? "UPDATE FUNCIONARIO SET EMAILFUNC = '" +
+                dados.Email +
+                `' WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }` +
+          `${
+            dados.Departamento
+              ? `UPDATE FUNCIONARIO SET CODDEP = (SELECT CODDEP FROM DEPARTAMENTO WHERE NOMEDEP ='${dados.Departamento}') WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }` +
+          `${
+            dados.Cargo
+              ? `UPDATE FUNCIONARIO SET CODCAR = (SELECT CODCAR FROM CARGO WHERE NOMECAR = '${dados.Cargo}') WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }` +
+          `${
+            dados.FUNCIONARIO
+              ? `UPDATE FUNCIONARIO SET CODGESTOR = (SELECT CODFUNC FROM FUNCIONARIO WHERE NOMEFUNC = '${dados.FUNCIONARIO}') WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }` +
+          `${
+            dados.TIPOFUNC
+              ? `UPDATE FUNCIONARIO SET TIPOID = (SELECT TIPOID FROM TIPOFUNC WHERE DESCRICAO = '${dados.TIPOFUNC}') WHERE CODFUNC = ${dados.codfunc} `
+              : ""
+          }`,
+
+        function (err, recordset) {
+          // console.log(request.query);
+          res.json(recordset.recordsets[0]);
+        }
+      );
+    })
+    .catch((err) => console.log("erro! " + err));
+});
+
+app.post("/usu", (req, res) => {
+  console.log(req.body);
+
+  let codpen = req.body.codpen;
+
+  sql
+    .connect(connStr)
+    .then((conn) => {
+      console.log("conectou!");
+
+      let request = new sql.Request();
+      request.query(
+        `SELECT FUNC.CODFUNC, FUNC.NOMEFUNC, FUNC.EMAILFUNC, NOMEDEP, NOMECAR, FUNC1.NOMEFUNC AS NOMEGESTOR, DESCRICAO
+        FROM FUNCIONARIO FUNC
+        INNER JOIN DEPARTAMENTO DEP ON DEP.CODDEP = FUNC.CODDEP
+        INNER JOIN CARGO CAR ON CAR.CODCAR = FUNC.CODCAR
+        LEFT JOIN FUNCIONARIO FUNC1 ON FUNC1.CODFUNC = FUNC.CODGESTOR
+        INNER JOIN TIPOFUNC FUN ON FUN.TIPOID = FUNC.TIPOID
+        `,
+        function (err, recordset) {
+          res.json(recordset.recordsets[0]);
+        }
+      );
+    })
+    .catch((err) => console.log("erro! " + err));
+});
 
 app.post("/iniciar_aval", (req, res) => {
   console.log(req.body);
@@ -315,6 +495,43 @@ app.post("/send_formulario", (req, res) => {
             })
             .catch((err) => console.log("erro! " + err));
           res.send(200);
+        }
+      );
+    })
+    .catch((err) => console.log("erro! " + err));
+});
+
+app.post("/campos", (req, res) => {
+  console.log(req.body);
+
+  let campo = req.body.campo;
+  let where = "WHERE TIPOID = 1";
+
+  let campos = "";
+
+  if (campo == "Departamento") {
+    campos = "NOMEDEP as label";
+  }
+  if (campo == "FUNCIONARIO") {
+    campos = "NOMEFUNC as label";
+    campo = campo + " " + where;
+  }
+  if (campo == "Cargo") {
+    campos = "NOMECAR as label";
+  }
+  if (campo == "TIPOFUNC") {
+    campos = "DESCRICAO as label";
+  }
+  sql
+    .connect(connStr)
+    .then((conn) => {
+      console.log("conectou!");
+
+      let request = new sql.Request();
+      request.query(
+        `SELECT ${campos} FROM ${campo}`,
+        function (err, recordset) {
+          res.json(recordset.recordsets[0]);
         }
       );
     })
